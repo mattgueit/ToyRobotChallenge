98d0ecd6ce814d864108650bf6967a7086817100
Matt Gueit <mattgueit@gmail.com>
2021-07-17 14:42:27 +1000

    Create new .NET 5 Console Application; add gitignore.

---
+++ ToyRobotChallenge.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToyRobotChallenge.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
